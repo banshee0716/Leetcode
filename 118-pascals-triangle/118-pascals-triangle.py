4f10b50e1509b8fb47a3abd277dd55bae0462aa9
NTNU_IDIOT <62146316+banshee0716@users.noreply.github.com>
2022-07-19 10:07:23 +0800

    Time: 14 ms (95.26%), Space: 13.4 MB (63.19%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def generate(self, numRows):
+        res = [[1]]
+        for i in range(1, numRows):
+            res += [map(lambda x, y: x+y, res[-1] + [0], [0] + res[-1])]
+        return res[:numRows]
