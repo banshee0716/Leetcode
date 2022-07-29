39e40b3ddf7f38a702ebca1e7e5170c77fa3ee49
NTNU_IDIOT <62146316+banshee0716@users.noreply.github.com>
2022-07-29 09:12:47 +0800

    Time: 21 ms (87.80%), Space: 13.5 MB (37.40%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findAndReplacePattern(self, words, p):
+        def F(w):
+            m = {}
+            return [m.setdefault(c, len(m)) for c in w]
+        return [w for w in words if F(w) == F(p)]
+        
