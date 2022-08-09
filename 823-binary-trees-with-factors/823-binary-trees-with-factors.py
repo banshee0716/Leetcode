b34473bd820bae5ac99adccde256a56ee3da96de
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2022-08-09 09:52:58 +0800

    Time: 114 ms (100.00%), Space: 14.1 MB (57.07%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.py
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -1,6 +1,13 @@
-class Solution(object):
-    def numFactoredBinaryTrees(self, A):
-        dp = {}
-        for a in sorted(A):
-            dp[a] = sum(dp[b] * dp.get(a / b, 0) for b in dp if a % b == 0) + 1
-        return sum(dp.values()) % (10**9 + 7)
+class Solution:
+    def numFactoredBinaryTrees(self, A: List[int]) -> int:
+        A.sort()
+        fmap, ans = defaultdict(), 0
+        for num in A:
+            ways, lim = 1, sqrt(num)
+            for fA in A:
+                if fA > lim: break
+                fB = num / fA
+                if fB in fmap:
+                    ways += fmap[fA] * fmap[fB] * (1 if fA == fB else 2)
+            fmap[num], ans = ways, (ans + ways)
+        return ans % 1000000007
