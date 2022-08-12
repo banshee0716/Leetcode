c84bec952c43a37acbb606912b3d9100da98c39c
banshee0716 <62146316+banshee0716@users.noreply.github.com>
2022-08-12 09:30:06 +0800

    no message

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree ITERATIVE.go
@@ -0,0 +1,12 @@
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	for root != nil {
+		if p.Val < root.Val && q.Val < root.Val {
+			root = root.Left
+		} else if p.Val > root.Val && q.Val > root.Val {
+			root = root.Right
+		} else {
+			return root
+		}
+	}
+	return nil
+}
