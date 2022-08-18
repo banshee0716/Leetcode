ce121de71083b5d37f6edf77ed75287e20eb1333
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2022-08-18 09:53:45 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (79.17%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.go
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.go
@@ -1,21 +1,22 @@
 func countHillValley(nums []int) int {
     var l ,res, idx int = len(nums), 0, 1
-    for idx < l - 1{
-		if nums[idx] == nums[idx - 1]{
-			idx++
-			continue
-		}
-		var right int = idx + 1
-		for right < l && nums[right] == nums[idx]{
-			right++
-		}
-		if right >= l{
-			break
-		}
-		if (nums[idx] > nums[idx - 1] && nums[idx] > nums[right]) || (nums[idx] < nums[idx - 1] && nums[idx] < nums[right]){
+    for idx < l-1{
+        if nums[idx] == nums[idx-1]{
+            idx++
+            continue
+            //處理如果都相同的edge case
+        }
+        var right int = idx + 1
+        for right < l && nums[right] == nums[idx]{
+            right++
+        }
+        if right >= l{
+            break
+        }
+        if (nums[idx] > nums[idx - 1] && nums[idx] > nums[right]) || (nums[idx] < nums[idx - 1] && nums[idx] < nums[right]){
 			res++
 		}
-		idx = right
-	}
-	return res
+        idx = right
+    }
+    return res
 }
