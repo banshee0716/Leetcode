5b34fc5e1055334fcaddd5ec1847a72db53d3a86
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2022-10-08 16:51:43 +0800

    Time: 98 ms (50.92%), Space: 5.8 MB (34.63%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.go
@@ -0,0 +1,34 @@
+func threeSumClosest(nums []int, target int) int {
+    if len(nums) < 3{
+        return -1
+    }
+    sort.Ints(nums)
+    smallestDiff := 1 << 31
+    for i := 0; i < len(nums); i++{
+        left, right := i+1, len(nums)-1
+        for left < right{
+            diff := target-nums[i] - nums[left] - nums[right]
+            if diff == 0{
+                return target
+            }
+            if abs (diff) < abs(smallestDiff)|| (abs(diff) == abs(smallestDiff) && diff > smallestDiff){
+            //把最小Diff修正回來，有兩個情境，一個是絕對值有差距，一個是絕對值相同但一個是正的差距
+                smallestDiff = diff
+            }
+            if diff > 0{
+                left++
+            }else{
+                right--
+            }
+        }
+    }
+    return target-smallestDiff
+}
+func abs (x int)int{
+    if x < 0{
+        return -x
+    }
+    return x
+}
+
+//O(n^2)
