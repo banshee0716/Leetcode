86ebe6d1b0028460889e76226108ae72ed7464a9
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2022-10-10 16:02:39 +0800

    Time: 41 ms (8.40%), Space: 13.5 MB (57.70%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def breakPalindrome(self, S):
+        for i in xrange(len(S) / 2):
+            if S[i] != 'a':
+                return S[:i] + 'a' + S[i + 1:]
+        return S[:-1] + 'b' if S[:-1] else ''
