1ccc96771f4e1846348237386f70aac8c8efad7a
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2022-10-12 15:47:26 +0800

    Time: 36 ms (98.99%), Space: 6.6 MB (88.89%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.go
@@ -0,0 +1,14 @@
+func largestPerimeter(nums []int) int {
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] > nums[j]
+	})
+	for i := 0; i < len(nums)-2; i++ {
+		a := nums[i]
+		b := nums[i+1]
+		c := nums[i+2]
+		if a < (b + c) {
+			return a + b + c
+		}
+	}
+	return 0
+}
