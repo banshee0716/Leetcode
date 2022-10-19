a2c7918a0002f7a726fb12cae0ee80634b26f94c
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2022-10-19 14:27:12 +0800

    Time: 55 ms (97.46%), Space: 13.9 MB (64.49%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,14 +1,6 @@
-from collections import defaultdict
-import heapq
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        cnt=defaultdict(lambda :[0,""])
-        for i in words:
-            cnt[i]=[cnt[i][0]-1,i]
-        lst=list(cnt.values())
-        heapq.heapify(lst)
-        lst=heapq.nsmallest(k,lst)
-        ans=[]
-        for i in lst:
-            ans.append(i[1])
-        return ans
+        c = Counter(words)
+        ans = sorted(c, key = lambda x:(-c[x],x))
+        
+        return ans[:k]
