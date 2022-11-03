4a3292019dd53dcf30ebe6bc6debe54dc5eb735c
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2022-11-03 14:12:31 +0800

    Time: 3705 ms (20.18%), Space: 36.2 MB (99.55%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        counter, ans = [[0]*26 for _ in range(26)], 0
+        for w in words:
+            a, b = ord(w[0]) - ord("a"), ord(w[1]) - ord("a")
+            if counter[b][a] :
+                ans += 4
+                counter[b][a] -= 1
+            else:
+                counter[a][b] += 1
+        
+        for i in range(26):
+            if counter[i][i]:
+                ans += 2
+                break
+                
+        return ans
