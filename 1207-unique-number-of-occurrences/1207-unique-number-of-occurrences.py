fb8ec12962fa3a7631032ba5eb5324966687bb33
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2022-11-30 13:25:26 +0800

    Time: 39 ms (91.14%), Space: 14.1 MB (34.11%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,10 +1,3 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        
-        # Get the occurrences of each number. (also remove duplicated numbers)
-        Number_Occurrences = Counter(arr)
-
-        Unique_Occurrences = set(Number_Occurrences.values())
-
-        # return Truen if each unique number has a unique occurrence
-        return len(Number_Occurrences.keys()) == len(Unique_Occurrences)
+        return len(Counter(arr).keys())==len(set(Counter(arr).values()))
