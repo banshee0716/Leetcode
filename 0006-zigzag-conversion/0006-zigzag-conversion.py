eb07ec657691b1dea389c1589b47e8cb5f1be70b
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-02-12 16:56:25 +0800

    Time: 57 ms (70.50%), Space: 14 MB (70.12%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -12,3 +12,8 @@ class Solution:
                 step = -1
             index += step
         return ''.join(rows)
+    
+    #chatGPT寫的
+    #首先，檢查字符串長度是否小於或等於numRows，或者numRows是否等於1，如果是，則返回原字符串。
+    #然後，使用一個列表rows存儲每一行的字符。使用兩個變量index和step分別跟蹤下一個字符應該放置在哪一行以及下一個字符應該往哪一個方向移動。
+    #在每次迭代中，將字符添加到適當的行，並將index和step更新為適當的值。最後，使用join函數將所有行連接起來並返回結果。
