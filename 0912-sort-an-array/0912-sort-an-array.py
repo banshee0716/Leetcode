d80ecad68a7c05fa694f9761689f5904e0d89b61
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-03-01 21:22:25 +0800

    Time: 1543 ms (77.25%), Space: 23.3 MB (26.39%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,3 +1,11 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return sorted(nums)
+        if len(nums) <= 1:
+            return nums
+
+        pivot = random.choice(nums)
+        lt = [v for v in nums if v < pivot]
+        eq = [v for v in nums if v == pivot]
+        gt = [v for v in nums if v > pivot]
+
+        return self.sortArray(lt) + eq + self.sortArray(gt)
