cbabc460eb4c6513068258f38f8356477ab4d184
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-03-08 20:48:44 +0800

    Time: 242 ms (60.30%), Space: 21.2 MB (67.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,27 +1,15 @@
-'''class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        ans = []
-        sum_nums = 1
-        for i in range(len(nums)):
-            sum_nums *= nums[i]#會溢位'''
-
-        
-
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prev = 1
         n = len(nums)
-        ans = [1]*n
-        for i in range(n):
-            ans[i] *= prev
-            prev *= nums[i]
-            
-        prev = 1
+        ans = [1] * n
         
-        for i in range(n-1, -1, -1):
-            ans[i] *= prev
-            prev *= nums[i]
+        left = 1
+        right = 1
+        
+        for i in range(len(nums)):
+            ans[i] *= left
+            ans[-1-i] *= right
+            left *= nums[i]
+            right *= nums[-1-i]
         
         return ans
-            
