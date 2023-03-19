547f45bab3975ad2c41c9ee5afae2d6433cf9967
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-03-19 21:22:54 +0800

    Time: 1041 ms (18.61%), Space: 13.9 MB (18.61%) - LeetHub

---
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+        def enough(num) -> bool:
+            count = 0
+            for val in range(1, m + 1):  # count row by row
+                add = min(num // val, n)
+                if add == 0:  # early exit
+                    break
+                count += add
+            return count >= k                
+
+        left, right = 1, n * m
+        while left < right:
+            mid = left + (right - left) // 2
+            if enough(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left 
+    
+    
