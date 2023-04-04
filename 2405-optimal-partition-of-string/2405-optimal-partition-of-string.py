3c22bf22fc5674b4990f4312ddc9db332a108165
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-04-04 14:19:58 +0800

    Time: 119 ms (57.12%), Space: 14.6 MB (47.10%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,11 +1,12 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        ans = 1
-        ls = []
-        for i in s:
-            if i in ls:
-                ls=[i]
-                ans+=1
-            else:
-                ls+=[i]
-        return ans
+        cur = set()
+        res = 1
+        
+        for c in s:
+            if c in cur:
+                cur = set()
+                res += 1
+            cur.add(c)
+                
+        return res
