d45a26cd2754751d80a512cec2db601050574a12
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-06-07 00:16:05 +0800

    Time: 61 ms (17.17%), Space: 16.6 MB (20.83%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        dif = arr[1]-arr[0]
+        for i in range(2, len(arr)):
+            if arr[i]-arr[i-1] != dif:
+                return False
+            dif = arr[i]-arr[i-1]
+        return True
+        #先sort之後走完整個陣列，開一個變數存差距之後一一比對
