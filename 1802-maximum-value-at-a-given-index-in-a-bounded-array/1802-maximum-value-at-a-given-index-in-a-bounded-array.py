094e4c1380f6cdaff87d1654c3b4a20b7e89c934
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-06-10 16:11:46 +0800

    Time: 44 ms (71.76%), Space: 16.2 MB (53.44%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.py
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -1,8 +1,10 @@
 class Solution:
     def check(self, a):
         left_offset = max(a - self.index, 0)
+        # 這裡計算左半部分數列的和，使用等差數列的求和公式
         result = (a + left_offset) * (a - left_offset + 1) // 2
         right_offset = max(a - ((self.n - 1) - self.index), 0)
+        # 這裡計算右半部分數列的和，使用等差數列的求和公式
         result += (a + right_offset) * (a - right_offset + 1) // 2
         return result - a
 
@@ -10,13 +12,26 @@ class Solution:
         self.n = n
         self.index = index
 
-        maxSum -= n
-        left, right = 0, maxSum
-        while left < right:
-            mid = (left + right + 1) // 2
-            if self.check(mid) <= maxSum:
+        maxSum -= n  # 減去每個數組元素最小的值
+        left, right = 0, maxSum  # 二分搜尋的左右邊界
+        while left < right:  # 進行二分搜尋
+            mid = (left + right + 1) // 2  # 計算中間值
+            if self.check(mid) <= maxSum:  # 如果中間值的和小於等於maxSum，則移動左邊界
                 left = mid
-            else:
+            else:  # 否則移動右邊界
                 right = mid - 1
-        result = left + 1
+        result = left + 1  # 最後找到的左邊界就是需要的最大值
         return result
+"""
+解題思路:
+
+首先，減去每個數組元素最小的值，使問題簡化為找出一個數組，該數組中每個元素都大於等於1，且其總和不超過maxSum。然後，使用二分搜尋法找出該數組中的最大值。
+
+時間複雜度:
+
+O(log(maxSum)) - 進行二分查找，所以時間複雜度為O(log(maxSum))。
+空間複雜度:
+
+O(1) - 這個演算法只使用了常數的額外空間。
+
+"""
