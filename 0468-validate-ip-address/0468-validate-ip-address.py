6fa5372f301416098c320d95add72577224702cd
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-06-16 15:57:17 +0800

    Time: 39 ms (72.90%), Space: 16.2 MB (77.41%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.py
+++ 0468-validate-ip-address/0468-validate-ip-address.py
@@ -1,16 +1,37 @@
 class Solution:
     def validIPAddress(self, IP: str) -> str:
-        
+        # 檢查一個子串是否是一個合法的IPv4地址的一部分
         def isIPv4(s):
-            try: return str(int(s)) == s and 0 <= int(s) <= 255
-            except: return False
+            try: 
+                # 轉換為整數後再轉回字串，如果與原來的字串相等且在0-255之間，則是合法的
+                return str(int(s)) == s and 0 <= int(s) <= 255
+            except: 
+                # 如果不能轉換為整數，則是不合法的
+                return False
 
+        # 檢查一個子串是否是一個合法的IPv6地址的一部分
         def isIPv6(s):
-            try: return len(s) <= 4 and int(s, 16) >= 0
-            except: return False
+            try: 
+                # 如果長度小於等於4且可以作為16進制數字，則是合法的
+                return len(s) <= 4 and int(s, 16) >= 0
+            except: 
+                # 如果不能轉換為16進制數字，則是不合法的
+                return False
 
+        # 根據"."和":"的數量決定是IPv4還是IPv6
         if IP.count(".") == 3 and all(isIPv4(i) for i in IP.split(".")):
             return "IPv4"
         if IP.count(":") == 7 and all(isIPv6(i) for i in IP.split(":")):
             return "IPv6"
         return "Neither"
+"""
+這題的問題是要求我們檢驗給定的字符串是否是合法的IPv4或IPv6地址。
+
+以下是詳細的解題思路：
+
+1. 定義了兩個輔助函數isIPv4()和isIPv6()來檢驗子串是否是合法的IPv4或IPv6地址的一部分。
+2. 利用str.count()函數和str.split()函數將給定的字符串切分成多個部分，並檢查是否符合IP地址的規則。
+3. 利用all()函數檢查是否所有的部分都滿足相應的規則。
+
+對於時間複雜度和空間複雜度，兩者都是O(1)，因為我們只是直接檢查輸入的字符串，並沒有使用任何與輸入大小相關的額外空間，且處理時間也不隨輸入大小的增長而增長。
+"""
