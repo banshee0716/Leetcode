23804c7021e30ad4dc0700dcd30278b1b5a50a5e
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-06-16 14:46:07 +0800

    Time: 1829 ms (36.44%), Space: 16.5 MB (63.93%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -1,48 +1,47 @@
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        #Calculate the number of unmatched left and right 
-
-        redundant_open=0
-        redundat_close=0
-        
+        # 計算需要移除的左括號和右括號的數量
+        redundant_open = 0
+        redundat_close = 0
         for i in s:
-            if i=="(":
-                redundant_open+=1
-            elif i==")":
-                if redundant_open>0:
-                    redundant_open-=1
+            if i == "(":
+                redundant_open += 1
+            elif i == ")":
+                if redundant_open > 0:
+                    redundant_open -= 1
                 else:
-				        # If we don't have a matching left, then this is a misplaced right, record it.
-
-                    redundat_close+=1
+                    # 如果沒有配對的左括號，則此右括號需要被移除
+                    redundat_close += 1
         
-        ans=set()
+        ans = set()  # 用來儲存所有可能的有效括號字符串
         
-        def dfs(index,left,right,ope,close,valid):
-		    # If we reached the end of the string, just check if the resulting expression is
-            # valid or not and also if we have removed the total number of left and right
-            # parentheses that we should have removed.
-            if index==len(s):
-                if left==right and ope==0 and close==0:
+        def dfs(index, left, right, ope, close, valid):
+            # 如果遍歷到字符串結尾，檢查當前的字符串是否為有效的括號字符串
+            if index == len(s):
+                if left == right and ope == 0 and close == 0:
                     ans.add(valid)
-                return 
+                return
             
-            if s[index]=='(':
-                if ope>0:
-                    dfs(index+1,left,right,ope-1,close,valid)
-                    
-                dfs(index+1,left+1,right,ope,close,valid+"(")
-                
-            elif s[index]==')':
-                if close>0:
-                    dfs(index+1,left,right,ope,close-1,valid)
-                if  right<left:  
-                    dfs(index+1,left,right+1,ope,close,valid+")")
+            # 對於每一個字符，都做出選擇：留下還是移除
+            if s[index] == '(':
+                if ope > 0:
+                    dfs(index + 1, left, right, ope - 1, close, valid)  # 移除此左括號
+                dfs(index + 1, left + 1, right, ope, close, valid + "(")  # 留下此左括號
+            elif s[index] == ')':
+                if close > 0:
+                    dfs(index + 1, left, right, ope, close - 1, valid)  # 移除此右括號
+                if right < left:
+                    dfs(index + 1, left, right + 1, ope, close, valid + ")")  # 留下此右括號
             else:
-                dfs(index+1,left,right,ope,close,valid+s[index])
+                dfs(index + 1, left, right, ope, close, valid + s[index])  # 非括號字符，直接留下
                 
-                    
-                
-        dfs(0,0,0,redundant_open,redundat_close,"")
+        dfs(0, 0, 0, redundant_open, redundat_close, "")
         return list(ans)
-     
+"""
+
+時間複雜度為
+O(n*2^n)，因為在最壞的情況下，我們可能需要探索輸入字符串的所有子字符串。其中，n為字符串的長度。
+
+空間複雜度為
+O(n)，主要是因為深度優先搜尋的遞迴調用棧和儲存最終結果的列表。
+"""
