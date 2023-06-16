81e60206713bbbda4d4a5806e6f2e23047fd34b6
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-06-16 15:54:30 +0800

    Time: 47 ms (40.86%), Space: 16.3 MB (40.25%) - LeetHub

---
+++ 0468-validate-ip-address/0468-validate-ip-address.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validIPAddress(self, IP: str) -> str:
+        
+        def isIPv4(s):
+            try: return str(int(s)) == s and 0 <= int(s) <= 255
+            except: return False
+
+        def isIPv6(s):
+            try: return len(s) <= 4 and int(s, 16) >= 0
+            except: return False
+
+        if IP.count(".") == 3 and all(isIPv4(i) for i in IP.split(".")):
+            return "IPv4"
+        if IP.count(":") == 7 and all(isIPv6(i) for i in IP.split(":")):
+            return "IPv6"
+        return "Neither"
