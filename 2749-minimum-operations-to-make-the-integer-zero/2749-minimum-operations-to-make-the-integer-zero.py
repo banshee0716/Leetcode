071b77df2ceb62ce70edc97c8e15b63c0f149fbd
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-07-01 16:03:37 +0800

    Time: 52 ms (49.84%), Space: 16.3 MB (40.80%) - LeetHub

--- 2749-minimum-operations-to-make-the-integer-zero/2749-minimum-operations-to-make-the-integer-zero.py
+++ 2749-minimum-operations-to-make-the-integer-zero/2749-minimum-operations-to-make-the-integer-zero.py
@@ -5,3 +5,17 @@ class Solution:
             if (num1 - k * num2).bit_count() <= k <= num1 - k * num2:
                 return k
         return -1
+
+"""
+Start from k = 1,
+to check if num = num1 - num2 * k can be the sum of k pow sof 2.
+
+The maximum operations is num, num = (2 ^ 0) * num.
+The minimum operations is num bits count.
+
+If num.bit_count() <= k <= num,
+then num can be the sum of k pows of 2.
+
+Time O(60log(num1 + num2))
+Space O(1)
+"""
