c69fdc4882e2129a0acc5c1088b7f2169d497ef7
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-07-05 00:56:56 +0800

    add another solution to 0137

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,13 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
         count = defaultdict(int)
-        
+
         for x in nums:
             count[x] += 1
 
         for x, freq in count.items():
             if freq == 1:
                 return x
-        
+
         return -1
+
+
+"""
+就是簡單的建立一個hash map之後，再透過hash map的特性來找出只出現一次的數字
+"""
