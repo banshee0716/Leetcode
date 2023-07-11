1e37df567378cff6022e3fc4153463c2009e63fb
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-07-11 14:14:55 +0800

    Time: 45 ms (93.43%), Space: 16.8 MB (59.10%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -1,47 +1,56 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        # Step 1: Build adjacency list graph
+        # 建立空的鄰接列表圖
         graph = {}
+        
+        # 用遞迴的方式建立二叉樹的鄰接列表圖
         self.buildGraph(root, None, graph)
 
-        # Step 2: Perform BFS from the target node
+        # 初始化廣度優先搜尋的隊列和訪問過的節點集合
         queue = [(target, 0)]
         visited = set([target])
+        
+        # 初始化結果列表
         result = []
         
+        # 開始進行廣度優先搜尋
         while queue:
+            # 從隊列中取出一個節點和它的距離
             node, distance = queue.pop(0)
             
+            # 如果這個節點的距離等於k，那麼把它加入結果列表
             if distance == k:
                 result.append(node.val)
-                
+            
+            # 如果距離大於k，則可以提前停止搜索
             if distance > k:
                 break
             
+            # 瀏覽當前節點的所有鄰居
             for neighbor in graph[node]:
+                # 如果這個鄰居節點還沒有被訪問過，那麼就把它加入隊列並標記為已訪問
                 if neighbor not in visited:
                     visited.add(neighbor)
                     queue.append((neighbor, distance + 1))
         
+        # 返回結果列表
         return result
-    
-    def buildGraph(self, node, parent, graph):
+
+    def buildGraph(self, node: TreeNode, parent: TreeNode, graph: Dict[TreeNode, List[TreeNode]]):
+        # 如果節點為空，則直接返回
         if not node:
             return
         
+        # 如果節點不在圖中，則在圖中為它建立一個新的條目
         if node not in graph:
             graph[node] = []
-            
+        
+        # 如果有父節點，那麼把父節點加入節點的鄰居列表，並把節點加入父節點的鄰居列表
         if parent:
             graph[node].append(parent)
             graph[parent].append(node)
-            
+        
+        # 遞歸地對左子樹和右子樹進行相同的操作
         self.buildGraph(node.left, node, graph)
         self.buildGraph(node.right, node, graph)
+
