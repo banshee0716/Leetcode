1298e13a3ef0b8ce50f3f8cee2073f51414cdafd
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-07-17 15:06:29 +0800

    Time: 80 ms (72.26%), Space: 16.2 MB (94.82%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1, num2 = 0, 0
+        while l1:
+            num1 = num1*10 + l1.val
+            l1 = l1.next
+        
+        
+        while l2:
+            num2 = num2*10 + l2.val
+            l2 = l2.next
+            
+        #製造鏈表 
+        dummylist = dummy = ListNode(0)
+        ans=str(num1+num2)
+        for i in ans:
+            dummy.next = ListNode(i)
+            dummy = dummy.next
+            
+        return dummylist.next
+        #分別把兩個數字加起來
+        #注意要寫一個進位儲存的
