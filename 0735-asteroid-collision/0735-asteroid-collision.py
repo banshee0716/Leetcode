7e3a21501189a2b76b9a911c52db3cee3d5df361
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-07-20 15:21:39 +0800

    Time: 117 ms (34.43%), Space: 17.6 MB (57.73%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,28 +1,25 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stack = []
-        for asteroid in asteroids:
-            if not stack or asteroid > 0:
-                stack.append(asteroid)
-            else:
-                while stack and stack[-1] > 0 and stack[-1] < abs(asteroid):
-                    stack.pop()
-                if stack and stack[-1] == abs(asteroid):
-                    stack.pop()
-                else:
-                    if not stack or stack[-1] < 0:
-                        stack.append(asteroid)
-                
-        return stack
+        stack = []  # 初始化堆疊
+        for asteroid in asteroids:  # 遍歷每個行星
+            if not stack or asteroid > 0:  # 如果堆疊為空或行星向右
+                stack.append(asteroid)  # 將行星加入到堆疊中
+            else:  # 否則
+                while stack and stack[-1] > 0 and stack[-1] < abs(asteroid):  # 如果堆疊非空且堆疊頂部的行星向右且小於當前行星的絕對值
+                    stack.pop()  # 彈出堆疊頂部的行星
+                if stack and stack[-1] == abs(asteroid):  # 如果堆疊非空且堆疊頂部的行星與當前行星絕對值相等
+                    stack.pop()  # 彈出堆疊頂部的行星
+                elif not stack or stack[-1] < 0:  # 如果堆疊為空或堆疊頂部的行星向左
+                    stack.append(asteroid)  # 將當前行星加入到堆疊中
+
+        return stack  # 返回堆疊中剩下的行星
+
             
         
-
-"""
-1. 我們得到一個整數數組 asteroids ，代表一行中的小行星。
-2. 對於每顆小行星，絕對值代表其大小，符號代表其方向（正值表示向右，負值表示向左）。每顆小行星都以相同的速度移動。
-3. 找出所有碰撞後小行星的狀態。如果兩顆小行星相遇，較小的一顆就會爆炸。如果兩者大小相同，則兩者都會爆炸。朝同一方向移動的兩顆小行星永遠不會相遇。     
-"""
 """
 1. 設定一個stack，之後在回圈中把所有>0（正向）的全部加進stack
 2. 如果沒有的話，就去計算他是否比現在stack裡面元素的絕對值大，結束就停下來
+
+時間複雜度是 O(n)，其中 n 是行星的數量，因為我們遍歷了每個行星且每個行星只會入堆疊或出堆疊一次。
+空間複雜度是 O(n)，因為在最壞情況下，所有的行星都會加入到堆疊中。
 """
