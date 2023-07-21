d0944caa9c723fc68fe1cd20cef8175a9e45fda4
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-07-22 03:09:47 +0800

    Time: 74 ms (38.82%), Space: 42.1 MB (37.27%) - LeetHub

---
+++ 2715-execute-cancellable-function-with-delay/2715-execute-cancellable-function-with-delay.js
@@ -0,0 +1,46 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    // call setTimeout, which is set to call fn after t amount of time
+    var timeout = setTimeout(() =>
+        fn(...args)
+    , t)
+
+    // define a clearTimeout
+    var cancelFn = () => clearTimeout(timeout);
+
+    // When/if we call the function, it will return cancelFn,
+    // and since the return line calls (and consequentially executes)
+    // cancelFn, timeout will be cancelled, thereby cancelling fn
+    return cancelFn;
+};
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const start = performance.now() 
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr))
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelT)
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *
+ *  setTimeout(() => {
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
+/*https://leetcode.com/problems/execute-cancellable-function-with-delay/discuss/3620608/Full-thorough-explanation-oror-simple-beginner-friendly */
