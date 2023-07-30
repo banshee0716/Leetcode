215e7820d21d486dc76d263776e787d43677f985
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-07-30 18:17:40 +0800

    Time: 44 ms (100.00%), Space: 16.3 MB (16.67%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        ans = 0
+        for h in hours:
+            if h >= target:
+                ans += 1
+        return ans
+       
+        """
+某公司有n名員工，編號從0到n-1。每個員工i在公司工作了hours[i]個小時。
+
+公司要求每位員工至少工作目標時間。
+
+給您一個長度為 n 的非負整數小時的 0 索引數組和一個非負整數目標。
+
+返回表示至少工作了目標時間的員工人數的整數。
+        """
