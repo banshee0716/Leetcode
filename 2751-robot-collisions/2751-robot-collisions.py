ecbc407e4508ebd1aa153bbbaa80dd3bcc27a7f0
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-08-04 16:48:28 +0800

    Time: 1081 ms (99.94%), Space: 34.2 MB (94.87%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.py
+++ 2751-robot-collisions/2751-robot-collisions.py
@@ -3,19 +3,26 @@ class Solution:
         
         n = len(positions)
         ind = sorted(range(n), key=positions.__getitem__)
+        print(ind)
         stack = []
         for i in ind:
-            if directions[i] == 'R':
+            if directions[i] == "R":
                 stack.append(i)
                 continue
-            while stack and h[i] > 0:
+            while stack and h[i]>0:
                 if h[stack[-1]] < h[i]:
                     h[stack.pop()] = 0
                     h[i] -= 1
                 elif h[stack[-1]] > h[i]:
-                    h[stack[-1]] -= 1
+                    h[stack[-1]] -= 1     
                     h[i] = 0
                 else:
                     h[stack.pop()] = 0
-                    h[i] = 0
+                    h[i] = 0 
+                    
         return [v for v in h if v > 0]
+    
+
+    """
+    維護一個stack，藉此去處理彼此元素彼此之間的碰撞關係
+    """
