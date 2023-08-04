39b34ed64ce1fde80d583f324df53c45d2ed864c
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-08-04 16:30:47 +0800

    Time: 1061 ms (99.94%), Space: 34.2 MB (94.87%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.py
@@ -0,0 +1,21 @@
+class Solution:
+    def survivedRobotsHealths(self, positions: List[int], h: List[int], directions: str) -> List[int]:
+        
+        n = len(positions)
+        ind = sorted(range(n), key=positions.__getitem__)
+        stack = []
+        for i in ind:
+            if directions[i] == 'R':
+                stack.append(i)
+                continue
+            while stack and h[i] > 0:
+                if h[stack[-1]] < h[i]:
+                    h[stack.pop()] = 0
+                    h[i] -= 1
+                elif h[stack[-1]] > h[i]:
+                    h[stack[-1]] -= 1
+                    h[i] = 0
+                else:
+                    h[stack.pop()] = 0
+                    h[i] = 0
+        return [v for v in h if v > 0]
