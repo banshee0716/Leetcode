fbc750f73c9ae986b4d9710be86a10890918fa16
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-09-01 16:05:14 +0800

    Time: 73 ms (78.67%), Space: 23.1 MB (65.29%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0]
+        for i in range(1, n+1):
+            res.append(res[i>>1] + i%2)
+            
+        return res
