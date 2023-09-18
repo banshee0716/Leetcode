3adf8936a4711a71f75c14985881c4b58dcf3daa
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-09-18 13:19:12 +0800

    Time: 101 ms (84.27%), Space: 16.6 MB (70.98%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -0,0 +1,12 @@
+import heapq
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        heap = []
+        for i, row in enumerate(mat):
+            strength = sum(row)
+            heappush(heap, (-strength, -i))
+            if len(heap) > k:
+                heappop(heap)
+        
+        return [-i for _, i in sorted(heap, reverse=True)]
+        #用minheap去處理，之後再來想要怎麼計算出每一行的參數（Sum()函數）
