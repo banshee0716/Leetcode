badc35e5c1306c75f098a3d9c01b5cd4c42a76d5
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-10-09 15:52:39 +0800

    Time: 90 ms (20.85%), Space: 17.6 MB (97.83%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -12,7 +12,7 @@ class Solution:
                     high = mid
                 
             return low
-        
+        # 一次找最左邊，另一個找下一個元素的最左邊－１＝前一個元素的最右邊
         first = search(target)
         last = search(target+1)-1
         
@@ -20,28 +20,10 @@ class Solution:
             return [first, last]
         else:
             return [-1, -1]
-        
+    
 """
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def search(x):
-            low,high=0,len(nums)
-            while low < high:
-                mid = (low + high) // 2
-                if nums[mid] < x:
-                    low = mid + 1
-                else:
-                    high = mid
-            
-            return low
-        
-        low = search(target)
-        high = search(target+1)-1
-        
-        if (low <= high):
-            return [low,high]
-        else:
-            return [-1,-1]
-
-
+時間複雜度分析：
+我們執行了兩次二分查找，每次的時間複雜度均為 O(logn)，因此整體時間複雜度也是 O(logn)。
+空間複雜度分析：
+我們只使用了常數級別的額外空間，因此空間複雜度為 O(1)。
 """
