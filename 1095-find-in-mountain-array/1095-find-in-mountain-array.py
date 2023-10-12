de3f0af46c1098e78572ca8429280978f0051e75
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-10-12 15:23:35 +0800

    Time: 38 ms (63.57%), Space: 17.1 MB (90.39%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -8,16 +8,18 @@
 
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
+        # 定義一個函數，用來找到山脈的頂點
         def findPeak(mountain_arr):
             left, right = 0, mountain_arr.length()-1
             while left < right:
                 mid = left + (right-left)//2
                 if mountain_arr.get(mid) < mountain_arr.get(mid + 1):
-                    left = mid+1
+                    left = mid + 1
                 else:
                     right = mid
             return left
         
+        # 定義一個二分查找函數
         def binary_search(left, right, is_increasing):
             while left <= right:
                 mid = left + (right - left) // 2
@@ -34,13 +36,17 @@ class Solution:
                         right = mid - 1
                     else:
                         left = mid + 1
-            return -1        
+            return -1
         
+        # 首先找到山頂        
         peak_index = findPeak(mountain_arr)
+        # 在山的左側進行二分查找
         result = binary_search(0, peak_index, True)
+        # 如果左側找不到，則在山的右側進行二分查找
         if result == -1:
             result = binary_search(peak_index + 1, mountain_arr.length() - 1, False)
         return result
+
         
         """
 （這個問題是交互問題。）
@@ -59,4 +65,20 @@ class Solution:
 - MountainArray.get(k) 傳回索引 k（從 0 開始索引）處的陣列元素。
 - MountainArray.length() 傳回數組的長度。
 調用 MountainArray.get 超過 100 次的提交將被判定為錯誤答案。此外，任何試圖規避法官的解決方案都將導致取消資格。
+        
+解題思路：      
+山脈數組是先嚴格遞增，然後嚴格遞減的數組。這個問題給定一個 MountainArray 接口，提供了兩個API：get(index) 用於取得下標為 index 的元素，以及 length() 用於獲取數組長度。並且給定一個目標值 target，要求我們找出這個目標值在山脈數組中的下標，如果存在多個，返回最小的下標，如果不存在，返回 -1。
+
+解題思路：
+1. 找到山頂元素：使用二分查找方法找到數組中的最大元素（山頂）。
+2. 在山的左側查找目標元素：在 0 到 山頂的區間進行正常的二分查找。
+3. 在山的右側查找目標元素：如果在左側沒有找到目標元素，則在山頂到數組末尾的區間進行反向的二分查找（因為是遞減的）。
+
+時間複雜度：
+找山頂：O(logN)，其中N 是 MountainArray 的長度。
+兩次二分查找：各自 O(logN)。
+所以總時間複雜度為：O(logN)。
+
+空間複雜度：
+由於我們只用到了常數個額外變數，所以空間複雜度為：O(1)。
         """
