6a6f5304067de0e23c72f6b432eb10f7dcc04199
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-11-05 15:17:50 +0800

    Time: 525 ms (67.93%), Space: 29.5 MB (94.02%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -1,33 +1,42 @@
+from typing import List
+
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
+        # 如果k為1，則贏家為前兩個玩家中的較強者
         if k == 1:
             return max(arr[0], arr[1])
         
-        if k> len(arr):
+        # 如果k大於陣列長度，則贏家為最強的玩家
+        if k > len(arr):
             return max(arr)
         
+        # 初始化當前贏家和連勝次數
         current_winner = arr[0]
         consecutive_wins = 0
+        
+        # 遍歷陣列中的玩家
         for i in range(1, len(arr)):
             if current_winner > arr[i]:
+                # 如果當前贏家繼續勝利，增加連勝次數
                 consecutive_wins += 1
             else:
+                # 如果當前玩家輸了，更新當前贏家並重設連勝次數
                 current_winner = arr[i]
                 consecutive_wins = 1
                 
+            # 如果連勝次數達到k，返回當前贏家
             if consecutive_wins == k:
                 return current_winner
-            
+        
+        # 如果沒有玩家連勝k次，返回最後的當前贏家
         return current_winner
-                
+
         
         
         """
-給定一個由不同整數組成的整數陣列 arr 和一個整數 k。
+時間複雜度分析：
+在最壞的情況下，遍歷一次陣列中的所有元素，時間複雜度為O(n)，其中n是陣列arr的長度。
 
-將在陣列的前兩個元素（即 arr[0] 和 arr[1]）之間進行遊戲。在每一輪遊戲中，我們將 arr[0] 與 arr[1] 進行比較，較大的整數獲勝並保持在位置 0，較小的整數移動到數組的末尾。當一個整數連續 k 輪獲勝時，遊戲結束。
-
-傳回將贏得比賽的整數。
-
-這場比賽一定會有勝者。
+空間複雜度分析：
+這段代碼只使用了固定的額外空間來儲存current_winner和consecutive_wins變量，因此空間複雜度為O(1)。
         """
