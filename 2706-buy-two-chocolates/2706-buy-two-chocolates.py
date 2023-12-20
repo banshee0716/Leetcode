6e4147a1f93ee86f7fc06c2feec8c91aaba392e5
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-12-20 11:21:55 +0800

    Time: 69 ms (34.60%), Space: 16.4 MB (23.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,3 +1,4 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        return money-sum(heapq.nsmallest(2, prices)) if money-sum(heapq.nsmallest(2, prices))>= 0 else money
+        return money-sum(heapq.nsmallest(2, prices)) if money-sum(heapq.nsmallest(2, prices)) >= 0 else money
+        
