d00369bdbdae2718d1bf74b95361dd431ed5f1c6
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2024-02-08 22:21:00 +0800

    Time: 101 ms (94.74%), Space: 17.6 MB (42.36%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,40 +1,47 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        # 初始化DP表
-        dp = [float('inf')] * (n + 1)
-        dp[0] = 0  # 0可以由0個完全平方數相加得到
+        # 創建小於等於n的所有完全平方數列表
+        squares = [i**2 for i in range(1, int(n**0.5) + 1)]
+        d, q, nq = 1, {n}, set()
         
-        # 填充DP表
-        for i in range(1, n + 1):
-            min_val = float('inf')
-            j = 1
-            
-            # 嘗試減去一個完全平方數並更新最小值
-            while j * j <= i:
-                min_val = min(min_val, dp[i - j * j] + 1)
-                j += 1
-            dp[i] = min_val
-        
-        # 返回組成n所需的最少完全平方數的數量
-        return dp[n]
+        while q:
+            for node in q:
+                for square in squares:
+                    if node == square:  # 直接找到答案
+                        return d
+                    if node < square:  # 無需繼續遍歷更大的完全平方數
+                        break
+                    nq.add(node - square)
+            q, nq, d = nq, set(), d + 1  # 更新變數，準備下一輪搜索
 
     
     """
+    
 解題思路
-這個問題可以通過動態規劃（Dynamic Programming, DP）來解決。基本思想是構建一個DP表dp，其中dp[i]表示整數i由最少的完全平方數相加組成的數量。
+這道題目可以透過廣度優先搜索（Breadth-First Search, BFS）來解決。首先，創建一個列表squares，裡面包含了所有小於等於n的完全平方數。然後，使用BFS來尋找最短路徑，即最少步驟到達n。
 
-初始化DP表：
-    -初始化dp表的大小為n+1，所有值設置為float('inf')（無窮大），代表初始時沒有任何數字可以由完全平方數的和組成。將dp[0]設為0，因為0可以由0個完全平方數相加得到。
+創建完全平方數列表：
+利用列表解析式創建一個包含所有小於等於n的完全平方數的列表squares。
 
-填充DP表：
-    -從1遍歷到n，對於每個數i，嘗試減去一個完全平方數j*j，查找最少的完全平方數的和的個數。這裡j從1開始，直到j*j小於等於i為止。
+初始化變數：
+d為深度（或步數），從1開始。
+q為當前層次的節點集合，初始只包含n。
+nq為下一層次的節點集合，初始化為空集合。
 
-返回結果：
-    -最終dp[n]中存儲的就是組成n所需的最少完全平方數的數量。
+BFS搜索：
+當q非空時，對於q中的每個節點node，遍歷squares列表。
+如果node等於某個完全平方數，則找到了答案，返回d。
+如果node小於某個完全平方數，則無需繼續遍歷更大的完全平方數。
+否則，將node-square加入到nq中。
+搜索結束後，更新q為nq，nq清空，並將d增加1。
     
 時間複雜度
-    -外層循環從1遍歷到n，內層循環在最壞情況下遍歷到sqrt(n)。因此，總的時間複雜度為O(n*sqrt(n))。
-    
+    -創建完全平方數列表的時間複雜度為O(√n)。
+    -BFS的最壞情況時間複雜度為O(n)，因為每一次減去一個完全平方數可能遍歷整個squares列表。
+    -總的時間複雜度為O(n√n)，因為每層BFS可能需要遍歷所有小於n的完全平方數。
+
 空間複雜度
-    -由於使用了一個長度為n+1的DP表，因此空間複雜度為O(n)。
+    -squares列表的空間複雜度為O(√n)。
+    -BFS過程中，集合q和nq的空間複雜度最壞情況下為O(n)。
+    -總的空間複雜度為O(n)。
     """
