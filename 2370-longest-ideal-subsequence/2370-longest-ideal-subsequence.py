e79ba80a211d1d23685547787de88a0804ec9056
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2024-04-25 12:50:08 +0800

    Time: 399 ms (69.63%), Space: 17.5 MB (28.89%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -1,11 +1,21 @@
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
-        dp = [0] * 26  # 初始化DP數組
+        # 初始化DP數組，每個位置對應26個英文字母的最長理想子序列長度
+        dp = [0] * 26
+        
         for ch in s:
-            i = ord(ch) - ord('a')  # 獲取當前字符對應的索引
-            # 更新dp[i]為其自身加上(i-k, i+k)範圍內的dp值的最大值
-            dp[i] = 1 + max(dp[max(0, i - k) : min(26, i + k + 1)])
-        return max(dp)  # 返回dp數組中的最大值
+            # 獲取當前字符對應的索引
+            i = ord(ch) - ord('a')
+            # 計算在動態規劃數組中可以考慮的範圍，確保不超出邊界
+            start = max(0, i - k)
+            end = min(26, i + k + 1)
+            # 更新dp[i]為其自身加上(i-k, i+k)範圍內的dp值的最大值加1
+            # 因為當前字符可以增加的最長子序列長度至少為1
+            dp[i] = 1 + max(dp[start:end])
+        
+        # 返回dp數組中的最大值，這是最長理想子序列的長度
+        return max(dp)
+
 
     """
 解題思路
