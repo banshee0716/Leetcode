2c53387c77aa73428610d81349b3b1409967b421
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2024-06-17 16:18:24 +0800

    Time: 84 ms (78.17%), Space: 16.4 MB (96.81%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -2,14 +2,19 @@ import math
 
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        # 遍歷可能的a值，範圍是從0到根號c
-        for a in range(int(math.sqrt(c)) + 1):
-            # 計算b的值為c - a^2的平方根
-            b = math.sqrt(c - a * a)
-            # 檢查b是否為整數，如果b的整數部分和自身相等，則b是整數
-            if b == int(b):
-                return True  # 如果b是整數，說明找到了合適的a和b
-        return False  # 如果遍歷完所有a都沒有找到合適的b，返回False
+        a, b = 0, int(math.sqrt(c))
+        
+        while a <= b:
+            cur = a * a + b * b
+            if cur == c:
+                return True
+            elif cur < c:
+                a += 1
+            else:
+                b -= 1
+        
+        return False
+
 
     """
 解題思路：
