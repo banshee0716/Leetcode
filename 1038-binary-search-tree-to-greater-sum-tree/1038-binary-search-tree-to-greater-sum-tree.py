ef47c63f1c01b59108106522d34c8951f7a1e791
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2024-06-25 23:23:37 +0800

    Time: 39 ms (34.73%), Space: 16.4 MB (96.70%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def bstToGst(self, root: TreeNode) -> TreeNode:
         self.val = 0
@@ -24,8 +25,10 @@ class Solution:
 給定二元搜尋樹 (BST) 的根，將其轉換為更大樹，使得原始 BST 的每個鍵都更改為原始鍵加上 BST 中大於原始鍵的所有鍵的總和。
 
 提醒一下，二元搜尋樹是滿足以下限制的樹：
+    -節點的左子樹僅包含鍵小於該節點鍵的節點。
+    -節點的右子樹僅包含鍵大於該節點鍵的節點。
+    -左子樹和右子樹也必須是二元搜尋樹。
+    
+解題思路：作一個 reverse in-order traversal，然後把值一路加上去
 
-節點的左子樹僅包含鍵小於該節點鍵的節點。
-節點的右子樹僅包含鍵大於該節點鍵的節點。
-左子樹和右子樹也必須是二元搜尋樹。
-    """
+"""
