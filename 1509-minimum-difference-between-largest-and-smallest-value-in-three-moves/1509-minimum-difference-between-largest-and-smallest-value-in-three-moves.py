2b90cbd70a2fc9ffab5bd03b7d7fdee55c0ba282
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2024-07-03 12:20:48 +0800

    Time: 283 ms (39.62%), Space: 28 MB (12.26%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -1,4 +1,19 @@
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 4:
+            return 0
         
-        return 0 if len(nums)<=4 else nums.sort() or min(nums[len(nums)-4+i]-nums[i] for i in range(4))
+        nums.sort()
+        min_diff = min(nums[n-1] - nums[3], nums[n-2] - nums[2], nums[n-3] - nums[1], nums[n-4] - nums[0])
+        
+        return min_diff
+        
+    
+    """
+給定一個整數數組 nums。
+
+一步操作，可以選擇 nums 的一個元素並將其變更為任意值。
+
+執行最多 3 次移動後，傳回 nums 的最大和最小值之間的最小差值。
+    """
