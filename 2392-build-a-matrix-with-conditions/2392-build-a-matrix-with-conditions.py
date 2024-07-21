98e8e818897bdebf64d0158031870594fffbb598
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2024-07-21 14:52:16 +0800

    Time: 584 ms (55.72%), Space: 25.4 MB (45.80%) - LeetHub

---
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -0,0 +1,29 @@
+class Solution:
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        
+        def fn(cond): 
+            """Return topological sort"""
+            graph = [[] for _ in range(k)]
+            indeg = [0]*k
+            for u, v in cond: 
+                graph[u-1].append(v-1)
+                indeg[v-1] += 1
+            queue = deque(u for u, x in enumerate(indeg) if x == 0)
+            ans = []
+            while queue: 
+                u = queue.popleft()
+                ans.append(u+1)
+                for v in graph[u]: 
+                    indeg[v] -= 1
+                    if indeg[v] == 0: queue.append(v)
+            return ans 
+        
+        row = fn(rowConditions)
+        col = fn(colConditions)
+        
+        if len(row) < k or len(col) < k: return []
+        ans = [[0]*k for _ in range(k)]
+        row = {x : i for i, x in enumerate(row)}
+        col = {x : j for j, x in enumerate(col)}
+        for x in range(1, k+1): ans[row[x]][col[x]] = x
+        return ans 
